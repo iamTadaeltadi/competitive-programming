32c91bcc6f7171665e2d48aeb01aee4bb06e262f
iamTadaeltadi <109872251+iamTadaeltadi@users.noreply.github.com>
2022-09-16 15:32:12 +0300

    Create Merge Intervals.py

---
+++ Merge Intervals.py
@@ -0,0 +1,9 @@
+class Solution:
+    def merge(self, intervals: [List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x:x[0])
+        res=[intervals[0]]
+        for i in range(1,len(intervals)):
+            if intervals[i][0]<=res[-1][1]:
+                res[-1][1]=max(intervals[i][1],res[-1][1])
+            else:res.append(intervals[i])
+        return res
