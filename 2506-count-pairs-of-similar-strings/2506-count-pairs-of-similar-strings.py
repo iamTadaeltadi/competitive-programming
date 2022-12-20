72211f66fc29c13e9e126135400556ec2fb1c490
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2022-12-20 22:57:22 +0300

    Time: 149 ms (64.88%), Space: 13.9 MB (6.49%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -5,9 +5,15 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in range(len(words)-1):
-            for j in range(i+1 , len(words)):
-                if set(words[i]) == set(words[j]):
-                    count +=1
-        return count 
+        d={}
+        for i in range(len(words)):
+            d[str(set(words[i]))]=1+d.get(str(set(words[i])),0)
+        for i in d.values():
+            if i>=2:
+                count+=((i-1)*(i))/2
+        return count
+
         
+            
+            
+             
