a2b281afb3c1dcea1dc300f83e03aefff0368511
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-01-18 21:36:27 +0300

    Time: 158 ms (72.89%), Space: 14.6 MB (95.91%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        if len(arr)<3:
+        if len(arr)<3:# if arr of length less than 3
             return False
 
         max_val=max(arr)
-        max_indx=arr.index(max_val)
-        print(max_indx)
-        check=True
+        max_indx=arr.index(max_val) #to check index of max value of array
+        check=True #i use two variable to check if the both block should be excuted
         c=True
         for i in range(1,len(arr)):
             
@@ -22,7 +21,7 @@ class Solution(object):
                 check=False
                 if arr[i]>=arr[i-1]:
                     return False
-        if  c or check:
+        if  c or check: #checking both block inside loop was excuted
             return False
         return True 
                 
