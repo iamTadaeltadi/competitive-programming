c690993e39c5b5faec909d8f2d24a05b5c466592
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-02-07 00:01:06 +0300

    Time: 43 ms (66.26%), Space: 13.8 MB (59.43%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        l,r=0,0 
+        x=[]
+        z=[]
+        while l<len(s):
+            if s[l] in s[r:]:
+                x.append(s[r])
+                r+=1
+            else:
+                s[l] not in s[r:]
+                l+=1
+            
+            if l==r:
+                z.append(len(x))
+                x=[] 
+            
+        return z
+            
+            
+            
+            
