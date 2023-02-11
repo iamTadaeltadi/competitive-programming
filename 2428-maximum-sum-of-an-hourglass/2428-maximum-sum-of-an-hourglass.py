4debb3ab73cdd3eea021f472b943ce4201b17d1c
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-02-11 21:51:10 +0300

    Time: 193 ms (85.33%), Space: 16 MB (29.33%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -1,17 +1,18 @@
-class Solution:
-    def maxSum(self, grid: List[List[int]]) -> int:
+class Solution(object):
+    def maxSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        x=0
         m=0
-        for i in range(len(grid)-2):
-            for j in range(len(grid[i])-2):
-                sum1=grid[i][j]+grid[i][j+1]+grid[i][j+2]
-                sum1+=grid[(i+1)][j+1]
-                sum1+=grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2]
-               
-                m=max(m,sum1)
-                
+        for i in range(len(grid[0])-2):
+            for j in range(len(grid)-2):
+                x=0
+                x+=grid[j][i] +grid[j][i+1]+grid[j][i+2]
+                x+=grid[j+1][i+1]
+                x+=grid[j+2][i] +grid[j+2][i+1]+grid[j+2][i+2]
+                m=max(x,m)
         return m
-                
-                
-
-
     
+        
