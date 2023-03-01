c2ad306e1816fbbc100d14788ce555e0f0961cf8
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-03-01 11:07:50 +0300

    Create hackerRank-arrayManipulation.py

---
+++ hackerRank-arrayManipulation.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    arr=[0 for i in range(n+1)]
+    for i in range(len(queries)):
+        left=queries[i][0]
+        right=queries[i][1]
+        k=queries[i][2]
+        arr[right]-=k
+        arr[left-1]+=k
+    for i in range(1,n+1):
+        arr[i]+=arr[i-1]
+    return max(arr)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
