dd0066d1f4334c2a0c25c6aed586e374d6572e86
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-03-06 20:55:23 +0300

    Time: 36 ms (40.82%), Space: 13.8 MB (55.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -10,19 +10,13 @@ class Solution:
     def pow(self,n):
         if n==0:
             return 1
-            if n<=1:
-                return self.x
-            if n%2==0:
-                return (self.pow(n/2))**(2)
-            else:
-                return (self.pow(n//2))**(2) *self.x
+            
+           
         else:
+           
             if n==-1 :
                 return 1/self.x
             if n%2==0:
                 return (self.pow(n/2))**(2)
             else:
                 return (self.pow(n//2))**(2) *self.x
-
-            
-        
