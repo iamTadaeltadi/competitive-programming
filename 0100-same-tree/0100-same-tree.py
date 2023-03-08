05f7b201736223b611054acef19b6934d68e9883
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-03-08 11:15:45 +0300

    Time: 28 ms (84.83%), Space: 13.8 MB (60.89%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,16 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.res=[]
+    
+class Solution(TreeNode):
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
