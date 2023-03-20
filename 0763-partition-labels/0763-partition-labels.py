403afe224bc91bdf845aafc6aea0e287fbe9c056
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-03-21 00:55:07 +0300

    Time: 80 ms (6.78%), Space: 13.9 MB (56.87%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,15 +1,19 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        
         count=Counter(s)
         curr={}
         res=[]
-        l=0
+        left=0
+        
         for right in range(len(s)):
             count[s[right]]-=1
             curr[s[right]]=count[s[right]]
+            
             if (all(item == 0 for item in curr.values())):
-                res.append(right-l+1)
-                l=right+1
+                res.append(right-left+1)
+                left=right+1
+                
         return res
             
                 
