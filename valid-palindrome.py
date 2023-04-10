06a4c3ec989ed9f69b2cb43a70e9ac9ca2fa3ba3
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-04-10 17:52:21 +0300

    added valid-palindrome.python

---
+++ valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        r=len(s)-1
+        l=0
+        while l<r:
+            while l<r and  not s[l].isalnum():
+                l+=1
+            while l<r and  not s[r].isalnum():
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1
+            
+        return True
