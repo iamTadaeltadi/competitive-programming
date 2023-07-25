6f2c9ed29bf9cdcb4311cc07936f019b8092e969
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-07-25 23:26:42 +0300

    added splitting-a-string-into-descending-consecutive-values.python

---
+++ splitting-a-string-into-descending-consecutive-values.py
@@ -0,0 +1,25 @@
+class Solution:
+    def splitString(self, s: str) -> bool:
+        res=[]
+        
+        
+        def splitter(index,res):
+            if index==len(s) - 1:
+                return False
+            res.append(int(s[:index + 1]))
+            val=s[index+1:]
+            
+            string=""
+            for i in val:
+                string+=i
+                if  ( res[0]!=0 and (res[-1]==0) and int(string)==0) or res[-1]- int(string)==1:
+                    
+                    res.append(int(string))
+                    string=''
+                    
+            if not string:
+                return True
+            else:
+                return splitter(index+1,[])
+        ress = splitter(0,[])
+        return ress
