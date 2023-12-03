be65ca0c2d3879a1e865e468212b41ea00cbcc04
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-12-03 07:33:39 +0300

    Added solution for Convert Sorted List to Binary Search Tree

--- leet-code-solutions/convert-sorted-list-to-binary-search-tree.py
+++ leet-code-solutions/convert-sorted-list-to-binary-search-tree.py
@@ -9,38 +9,32 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-
-
-        to_list = []
-
-        while head:
-            to_list.append(head.val)
-            head = head.next
-        
-        
-
-        n = len(to_list)
-
-        def inbound(point) :
-            return 0< point < n 
-
-        
-        
-        def dfs(l,r):
-            mid = l + (r-l)//2
-            
-
-            if r<l :
+        def build_bst(l, r):
+            if l > r:
                 return None
-            root = TreeNode(to_list[mid])
+            
+            mid = (l + r) // 2
 
-            
-            root.left = dfs(l,mid-1)
-            root.right = dfs(mid+1,r)
-            
+            left_subtree = build_bst(l, mid - 1)
+
+            nonlocal head
+            root = TreeNode(val=head.val)
+            root.left = left_subtree
+
+            head = head.next  # Move to the next element in the linked list
+
+            root.right = build_bst(mid + 1, r)
 
             return root
 
-        return dfs(0,n-1)
+        # Count the number of elements in the linked list
+        n = 0
+        current = head
+        while current:
+            n += 1
+            current = current.next
+
+        return build_bst(0, n - 1)
