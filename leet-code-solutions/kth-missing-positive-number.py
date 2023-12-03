536da97976989ad3e3f6cd94959fc865c1947195
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-12-03 07:13:41 +0300

    Added solution for Kth Missing Positive Number

---
+++ leet-code-solutions/kth-missing-positive-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findKthPositive(self, arr: list[int], k: int) -> int:
+        l, r = 0, len(arr)
+        while l < r:
+            m = (l + r) // 2
+            l, r = (l, m) if arr[m] - (m + 1) >= k else (m + 1, r)
+        return l + k
