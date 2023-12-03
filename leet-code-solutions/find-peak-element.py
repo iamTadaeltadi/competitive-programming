97ea4b1d8e83bb2a34bcb37a505be424f7117cae
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-12-03 07:25:20 +0300

    Added solution for Find Peak Element

--- leet-code-solutions/find-peak-element.py
+++ leet-code-solutions/find-peak-element.py
@@ -1,28 +1,76 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        n=len(nums)
-        if n==1:
+
+
+
+        left = [nums[0]]
+
+
+        for i in range(1,len(nums)):
+            left.append(max(nums[i],(left[-1])))
+
+
+
+        right = [nums[-1]]
+
+        for i in range(len(nums)-2,-1,-1):
+            right.append(max(nums[i],(right[-1])))
+        
+
+        l = 0
+        r = len(nums) -1
+
+        if len(nums) ==1:
             return 0
-        low=0
-        high=len(nums)-1
-        while low<=high:
-            mid=low+(high-low)//2
-            if mid==0:
-                if nums[mid]>nums[mid+1]:
-                    return mid
-                else:
-                    low=mid+1
-            elif mid==n-1:
-                if nums[mid]>nums[mid-1]:
-                    return mid
-                else:
-                    high=mid-1
-            else:
-                if nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1]:
-                    return mid
-                else:
-                    if nums[mid]<nums[mid+1]:
-                        low=mid+1
-                    else:
-                        high=mid-1
+        
+        right.reverse()
+
+        print(left,right)
+
+        while l<=r:
+            mid = (l) +(r-l)//2
+            print(mid)
+
+            if mid>0 and mid <len(nums)-1 and nums[mid] >= left[mid-1] and nums[mid] >= right[mid+1]:
+                return mid
+            
+
+
+            if mid ==0 and len(nums)>=2 and  nums[mid] >= right[mid+1] :
+                print("x")
+                return 0
+
+            if mid == len(nums)-1 and len(nums)>=2 and  nums[mid] >= left[mid-1] :
+                return len(nums)-1
+            
+
+            if mid ==0:
+                print("x")
+                l = mid+1
+            
+            elif mid == len(nums)-1 :
+                r = mid -1
+            elif nums[mid] < left[mid-1] >= right[mid+1] :
+                r = mid-1
+
+            elif nums[mid] < right[mid+1] >= left[mid-1] :
+                l = mid +1
+
+        
         return -1
+
+    
+            
+
+            
+
+
+
+
+        
+
+
+        
+
+       
+        
