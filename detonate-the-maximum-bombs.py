011bc23ec224395609efb4887bcb911f669ff6c1
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2024-02-22 07:04:23 +0300

    added detonate-the-maximum-bombs.python

--- detonate-the-maximum-bombs.py
+++ detonate-the-maximum-bombs.py
@@ -1,27 +1,35 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        graph=defaultdict(list)
+
+        graph = defaultdict(list)
+        
+
+        
         for i in range(len(bombs)):
+            radius = bombs[i][2]
+            x, y = bombs[i][0]  , bombs[i][1]  
             for j in range(len(bombs)):
-                if i!=j:
-                    h,k,r=bombs[i][0],bombs[i][1],bombs[i][2]
-                    x,y=bombs[j][0],bombs[j][1]
-                    
-                    dis=math.sqrt((x-h)**2+(y-k)**2)
-                    if dis<=r:
+                if i != j:
+                
+                    eculidian_distance = math.pow((x - bombs[j][0])**2 + (y - bombs[j][1])**2,1/2)
+                
+                    if eculidian_distance <= radius  :
                         graph[i].append(j)
         count=1
-        print(graph)
+        
         for i in range(len(bombs)):
             stack=[i]
+            
             visited=set()
             c=1
             while stack:
                 curr=stack.pop()
+                
                 visited.add(curr)
                 for neg in graph[curr]:
                     if neg not in visited:
                         stack.append(neg)
                         c+=1
+            
             count=max(count,len(visited))
         return count
