5bc60902382a79219869b840b113c7360ee2884d
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2024-03-05 23:51:16 +0300

    added minimum-length-of-string-after-deleting-similar-ends.python

---
+++ minimum-length-of-string-after-deleting-similar-ends.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumLength(self, s):
+        left, right = 0, len(s) - 1
+        
+        while left < right and s[left] == s[right]:
+            char = s[left]
+            while left <= right and s[left] == char:
+                left += 1
+            while right >= left and s[right] == char:
+                right -= 1
+        
+        return right - left + 1
